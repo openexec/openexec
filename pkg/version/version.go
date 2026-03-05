258a40e8ef4f15b5d0279332a7c3fcb7581e3bf6
Your Name <you@example.com>
2026-03-05 23:20:20 +0200

    chore: Bump version to v0.1.5 to align with release dashboard

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the current version of OpenExec
-const Version = "0.1.0"
+const Version = "0.1.5"
 
 // Commit is the git commit hash, usually injected at build time
 var Commit = "none"
