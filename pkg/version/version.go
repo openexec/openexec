2783e1081358931daec3a240329e6cfebc3d9fa9
Your Name <you@example.com>
2026-03-12 21:05:10 +0200

    fix(version): bump to v0.3.2 in core package

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the current version of OpenExec
-const Version = "0.3.1"
+const Version = "0.3.2"
 
 // Commit is the git commit hash, usually injected at build time
 var Commit = "none"
